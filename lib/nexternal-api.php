7436dc0bfa9b6065f101c481b0cbada88b48a47a
AlreadySetUp <AlreadySetUp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-20 21:15:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nexternal/trunk@399413 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/nexternal-api.php
@@ -0,0 +1,134 @@
+<?php
+
+function generateProductQueryRequest($accountName, $key) {
+    $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<ProductQueryRequest>
+  <Credentials>
+    <AccountName>$accountName</AccountName>
+    <Key>$key</Key>
+  </Credentials>
+  <ProductNoRange>
+    <ProductNoStart>0</ProductNoStart>
+    <ProductNoEnd>999999999</ProductNoEnd>
+  </ProductNoRange>
+  <CurrentStatus />
+</ProductQueryRequest>
+XML;
+    return $xml;
+}
+
+function generateTestSubmitRequest($accountName, $username, $password) {
+    $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<TestSubmitRequest>
+  <Credentials>
+    <AccountName>$accountName</AccountName>
+    <UserName>$username</UserName>
+    <Password>$password</Password>
+  </Credentials>
+</TestSubmitRequest>
+XML;
+    return $xml;
+}
+
+function generateTestVerifyRequest($accountName, $username, $password, $testKey, $testKeyLocation) {
+
+    if ($testKeyLocation == 'Attribute') {
+
+        $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<TestVerifyRequest>
+  <Credentials Key="$testKey">
+    <AccountName>$accountName</AccountName>
+    <UserName>$username</UserName>
+    <Password>$password</Password>
+  </Credentials>
+</TestVerifyRequest>
+XML;
+
+    } else { // node
+
+        $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<TestVerifyRequest>
+  <Credentials>
+    <AccountName>$accountName</AccountName>
+    <UserName>$username</UserName>
+    <Password>$password</Password>
+    <Key>$testKey</Key>
+  </Credentials>
+</TestVerifyRequest>
+XML;
+
+    }
+
+    return $xml;
+
+}
+
+function generateProductQuery($accountName, $key, $SKU) {
+
+    return <<<XML
+<?xml version="1.0" encoding="utf-8" ?>
+<ProductQueryRequest>
+  <Credentials>
+    <AccountName>$accountName</AccountName>
+    <Key>$key</Key>
+  </Credentials>
+  <ProductSKU>$SKU</ProductSKU>
+  <IncludeReviews></IncludeReviews>
+</ProductQueryRequest>
+XML;
+
+}
+
+
+function curl_post($url, $xml, $test=false) {
+
+    if ($test) echo "sending:\n$xml\nto: $url\n\n";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $xml); 
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    if ($test) echo "received response:\n$output\n\n";
+
+    return $output;
+}
+
+function nexternal_getActiveKey($accountName, $username, $password) {
+
+    // send TestSubmitRequest
+    $url = "https://www.nexternal.com/shared/xml/testsubmit.rest";
+    $xml = generateTestSubmitRequest($accountName, $username, $password);
+    $xmlResponse = curl_post($url, $xml);
+
+    // get key from TestSubmitReply
+    $xmlData = new SimpleXMLElement($xmlResponse);
+    $testKey = $xmlData->TestKey;
+    $attributes = $xmlData->attributes();
+    $testKeyLocation = $attributes['Type'];
+
+    // send TestVerifyRequest
+    $url = "https://www.nexternal.com/shared/xml/testverify.rest";
+    $xml = generateTestVerifyRequest($accountName, $username, $password, $testKey, $testKeyLocation);
+    $xmlResponse = curl_post($url, $xml);
+
+    // get activeKey from TestVerifyReply
+    $xmlData = new SimpleXMLElement($xmlResponse);
+    $activeKey = $xmlData->ActiveKey;
+
+    //echo "ActiveKey received: $activeKey\n\n";
+
+    return $activeKey . '';
+}
+
+
+?>
---
+++ tinymce/editor_plugin.js
@@ -0,0 +1,64 @@
+// Docu : http://wiki.moxiecode.com/index.php/TinyMCE:Create_plugin/3.x#Creating_your_own_plugins
+
+(function() {
+	// Load plugin specific language pack
+	tinymce.PluginManager.requireLangPack('nexternalPlugin');
+	
+	tinymce.create('tinymce.plugins.nexternalPlugin', {
+		/**
+		 * Initializes the plugin, this will be executed after the plugin has been created.
+		 * This call is done before the editor instance has finished it's initialization so use the onInit event
+		 * of the editor instance to intercept that event.
+		 *
+		 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+		 * @param {string} url Absolute URL to where the plugin is located.
+		 */
+		init : function(ed, url) {	
+			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
+
+			ed.addCommand('mcenexternalPlugin', function() {
+				ed.windowManager.open({
+					file : url + '/window.php',
+					width : 400 + ed.getLang('nexternalPlugin.delta_width', 0),
+					height : 475 + ed.getLang('nexternalPlugin.delta_height', 0),
+					inline : 1
+				}, {
+					plugin_url : url // Plugin absolute URL
+				});
+			});
+
+			// Register example button
+			ed.addButton('nexternalPlugin', {
+				title : 'Add Nexternal Product List',
+				cmd : 'mcenexternalPlugin',
+				image : url + '/nexternalPlugin.gif'
+			});
+
+			// Add a node change handler, selects the button in the UI when a image is selected
+			ed.onNodeChange.add(function(ed, cm, n) {
+				cm.setActive('nexternalPlugin', n.nodeName == 'IMG');
+			});
+		},
+
+		/**
+		 * Returns information about the plugin as a name/value array.
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 *
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
+		getInfo : function() {
+			return {
+					longname  : 'Nexternal Product List Options',
+					author 	  : 'Deanna Schneider',
+					authorurl : 'http://deannaschneider.wordpress.com',
+					infourl   : 'http://deannaschneider.wordpress.com',
+					version   : "1.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('nexternalPlugin', tinymce.plugins.nexternalPlugin);
+})();
+
+
